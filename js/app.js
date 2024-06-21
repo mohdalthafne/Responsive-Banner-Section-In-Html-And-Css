ab23ca4da471598c1bc53c4f5cfe9790c9d2be0d
Mr Web X <104372337+mohdalthafne@users.noreply.github.com>
2024-06-21 16:34:14 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+let menuBar = document.querySelector('header .navbar');
+    menuBtn = document.querySelector('header .icons .fa-ellipsis-vertical').onclick = () =>{
+        menuBar.classList.toggle('show');
+    }
